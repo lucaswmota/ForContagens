91d8611d259ec00c4013d1fd4e1c91631e85caf9
Lucas Mota <lucasmota7@gmail.com>
2021-12-01 20:44:39 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForContagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contando até 100 em modo crescente");
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Contando de 100 a 1 em modo Decrescente");
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Contando somente os pares até 100");
+            for (int i = 0; i <= 100; i += 2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Contando os Ímpares até 100");
+            for (int i = 1; i <= 100; i += 2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
